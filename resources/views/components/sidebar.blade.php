4adb91b5ffa6ebdfe7dd53a5ff1fc438b74b75ff
Martin Erren <martin3r@Mac.fritz.box>
2025-10-05 20:20:03 +0200

    Init

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,15 +4,28 @@
         :class="collapsed ? 'w-16' : 'w-72'" 
         class="shrink-0 h-screen bg-black border-r border-[color:var(--ui-border)] transition-all duration-300 flex flex-col overflow-y-auto"
     >
-        <!-- Toggle Button (öffnet Modul-Modal) -->
+        <!-- Header: Modul-Trigger + Toggle -->
         <div class="sticky top-0 z-10 bg-black border-b border-[color:var(--ui-border)] p-2">
+            <!-- Modul-Modal öffnen -->
             <button 
                 @click="$dispatch('open-modal-modules')" 
-                class="flex items-center justify-center transition bg-black hover:bg-[rgba(255,255,255,0.06)] w-full"
-                :class="collapsed ? 'w-12' : 'w-full'"
+                class="flex items-center justify-center transition bg-black hover:bg-[rgba(255,255,255,0.06)] w-full mb-2"
                 title="Module öffnen"
             >
                 @svg('heroicon-o-squares-2x2', 'w-6 h-6 text-white')
+            </button>
+            <!-- Sidebar ein-/ausklappen -->
+            <button 
+                @click="toggle()" 
+                class="flex items-center justify-center transition bg-black hover:bg-[rgba(255,255,255,0.06)] w-full"
+                :title="collapsed ? 'Sidebar ausklappen' : 'Sidebar einklappen'"
+            >
+                <template x-if="collapsed">
+                    @svg('heroicon-o-chevron-double-right', 'w-6 h-6 text-white')
+                </template>
+                <template x-if="!collapsed">
+                    @svg('heroicon-o-chevron-double-left', 'w-6 h-6 text-white')
+                </template>
             </button>
         </div>
 
