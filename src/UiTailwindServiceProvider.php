fe5ebccde3ce3b6934c24584e215daa9a89cf014
Martin Erren <martin3r@Mac.fritz.box>
2025-10-05 16:19:55 +0200

    Init

--- src/UiTailwindServiceProvider.php
+++ src/UiTailwindServiceProvider.php
@@ -20,18 +20,45 @@ class UiTailwindServiceProvider extends ServiceProvider
             __DIR__ . '/../config/ui.php' => config_path('ui.php'),
         ], 'ui-tailwind-config');
 
-        // Komponenten vorerst unter neuem Namespace registrieren
+        // Komponenten unter neuem Namespace registrieren
         Blade::component('ui-tailwind::components.ui-styles', 'ui-styles');
+        Blade::component('ui-tailwind::components.button', 'ui-button');
+        Blade::component('ui-tailwind::components.badge', 'ui-badge');
+        Blade::component('ui-tailwind::components.label', 'ui-label');
+
         Blade::component('ui-tailwind::components.sidebar', 'ui-sidebar');
+        Blade::component('ui-tailwind::components.right-sidebar', 'ui-right-sidebar');
+        Blade::component('ui-tailwind::components.sidebar-module-header', 'sidebar-module-header');
+        Blade::component('ui-tailwind::components.task-layout', 'ui-task-layout');
+
+        Blade::component('ui-tailwind::components.modal', 'ui-modal');
+        Blade::component('ui-tailwind::components.confirm-button', 'ui-confirm-button');
+
         Blade::component('ui-tailwind::components.table', 'ui-table');
         Blade::component('ui-tailwind::components.table-header', 'ui-table-header');
         Blade::component('ui-tailwind::components.table-header-cell', 'ui-table-header-cell');
         Blade::component('ui-tailwind::components.table-body', 'ui-table-body');
         Blade::component('ui-tailwind::components.table-row', 'ui-table-row');
         Blade::component('ui-tailwind::components.table-cell', 'ui-table-cell');
-        Blade::component('ui-tailwind::components.modal', 'ui-modal');
-        Blade::component('ui-tailwind::components.button', 'ui-button');
-        Blade::component('ui-tailwind::components.badge', 'ui-badge');
+
+        Blade::component('ui-tailwind::components.tab', 'ui-tab');
+        Blade::component('ui-tailwind::components.grouped-list', 'ui-grouped-list');
+        Blade::component('ui-tailwind::components.grouped-list-item', 'ui-grouped-list-item');
+        Blade::component('ui-tailwind::components.toast', 'ui-toast');
+        Blade::component('ui-tailwind::components.dashboard-tile', 'ui-dashboard-tile');
+
+        // Form Inputs
+        Blade::component('ui-tailwind::components.form.input-text', 'ui-input-text');
+        Blade::component('ui-tailwind::components.form.input-textarea', 'ui-input-textarea');
+        Blade::component('ui-tailwind::components.form.input-select', 'ui-input-select');
+        Blade::component('ui-tailwind::components.form.input-checkbox', 'ui-input-checkbox');
+        Blade::component('ui-tailwind::components.form.input-date', 'ui-input-date');
+        Blade::component('ui-tailwind::components.form.input-number', 'ui-input-number');
+
+        // Kanban
+        Blade::component('ui-tailwind::components.kanban.board', 'ui-kanban-board');
+        Blade::component('ui-tailwind::components.kanban.column', 'ui-kanban-column');
+        Blade::component('ui-tailwind::components.kanban.card', 'ui-kanban-card');
     }
 
     public function register(): void
