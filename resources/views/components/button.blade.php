041d0b366f30b70be7a8ca5be2bd0ac51e765a65
Martin Erren <martin3r@Mac.fritz.box>
2025-10-06 20:30:25 +0200

    Init

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -5,6 +5,7 @@
     'rounded' => 'md',          // none, sm, md, lg, full
     'iconOnly' => false,
     'href' => null,
+    'useVars' => true,          // wenn false: feste Tailwind-Farben für Diagnose/Fail-Safe
 ])
 
 @php
@@ -19,30 +20,68 @@
         }
     }
 
-    // Farbkonstrukte über CSS-Variablen; korrekte Tailwind Arbitrary Values
-    if ($isOutline) {
-        $bgClass = 'bg-transparent';
-        $textClass = "text-[var(--ui-{$baseVariant})]";
-        $borderClass = "border border-solid border-[rgb(var(--ui-{$baseVariant}-rgb))]";
-        $hoverClass = "hover:bg-[rgb(var(--ui-{$baseVariant}-rgb))] hover:text-[var(--ui-on-{$baseVariant})]";
-    } elseif ($isSoft) {
-        $bgClass = "bg-[rgba(var(--ui-{$baseVariant}-rgb),0.10)]";
-        $textClass = "text-[var(--ui-{$baseVariant})]";
-        $borderClass = "border border-[rgba(var(--ui-{$baseVariant}-rgb),0.25)]";
-        $hoverClass = "hover:bg-[rgba(var(--ui-{$baseVariant}-rgb),0.16)] hover:border-[rgba(var(--ui-{$baseVariant}-rgb),0.35)]";
-    } elseif ($isGhost) {
-        $bgClass = 'bg-transparent';
-        $textClass = "text-[var(--ui-{$baseVariant})]";
-        $borderClass = "border border-transparent";
-        $hoverClass = "hover:bg-[rgba(var(--ui-{$baseVariant}-rgb),0.08)] hover:border-[rgba(var(--ui-{$baseVariant}-rgb),0.20)]";
+    // Farb-Setup: wahlweise via CSS-Variablen (normal) oder feste Tailwind-Farben (Diagnose)
+    $useVarsBool = filter_var($useVars, FILTER_VALIDATE_BOOLEAN);
+    if ($useVarsBool) {
+        if ($isOutline) {
+            $bgClass = 'bg-transparent';
+            $textClass = "text-[var(--ui-{$baseVariant})]";
+            $borderClass = "border border-solid border-[rgb(var(--ui-{$baseVariant}-rgb))]";
+            $hoverClass = "hover:bg-[rgb(var(--ui-{$baseVariant}-rgb))] hover:text-[var(--ui-on-{$baseVariant})]";
+        } elseif ($isSoft) {
+            $bgClass = "bg-[rgba(var(--ui-{$baseVariant}-rgb),0.10)]";
+            $textClass = "text-[var(--ui-{$baseVariant})]";
+            $borderClass = "border border-[rgba(var(--ui-{$baseVariant}-rgb),0.25)]";
+            $hoverClass = "hover:bg-[rgba(var(--ui-{$baseVariant}-rgb),0.16)] hover:border-[rgba(var(--ui-{$baseVariant}-rgb),0.35)]";
+        } elseif ($isGhost) {
+            $bgClass = 'bg-transparent';
+            $textClass = "text-[var(--ui-{$baseVariant})]";
+            $borderClass = "border border-transparent";
+            $hoverClass = "hover:bg-[rgba(var(--ui-{$baseVariant}-rgb),0.08)] hover:border-[rgba(var(--ui-{$baseVariant}-rgb),0.20)]";
+        } else {
+            $bgClass = "bg-[rgb(var(--ui-{$baseVariant}-rgb))]";
+            $textClass = "text-[var(--ui-on-{$baseVariant})]";
+            $borderClass = '';
+            $hoverClass = "hover:opacity-90";
+        }
+        $focusRing = "focus:outline-none focus:ring-2 focus:ring-[rgba(var(--ui-{$baseVariant}-rgb),0.28)] focus:ring-offset-1 focus:ring-offset-[var(--ui-surface)]";
     } else {
-        // solid
-        $bgClass = "bg-[rgb(var(--ui-{$baseVariant}-rgb))]";
-        $textClass = "text-[var(--ui-on-{$baseVariant})]";
-        $borderClass = '';
-        $hoverClass = "hover:opacity-90";
+        // Feste Tailwind-Farben (nur für Diagnose): Mapping ausgewählter Varianten
+        $colorMap = [
+            'primary' => 'indigo',
+            'success' => 'emerald',
+            'secondary' => 'slate',
+            'info' => 'sky',
+            'warning' => 'amber',
+            'danger' => 'rose',
+        ];
+        $c = $colorMap[$baseVariant] ?? 'indigo';
+        if ($isOutline) {
+            $bgClass = 'bg-transparent';
+            $textClass = "text-{$c}-600";
+            $borderClass = "border border-{$c}-600";
+            $hoverClass = "hover:bg-{$c}-600 hover:text-white";
+            $focusRing = "focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{$c}-600";
+        } elseif ($isSoft) {
+            $bgClass = "bg-{$c}-100";
+            $textClass = "text-{$c}-700";
+            $borderClass = "border border-{$c}-200";
+            $hoverClass = "hover:bg-{$c}-200";
+            $focusRing = "focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{$c}-600";
+        } elseif ($isGhost) {
+            $bgClass = 'bg-transparent';
+            $textClass = "text-{$c}-600";
+            $borderClass = "border border-transparent";
+            $hoverClass = "hover:bg-{$c}-50";
+            $focusRing = "focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{$c}-600";
+        } else {
+            $bgClass = "bg-{$c}-600";
+            $textClass = 'text-white';
+            $borderClass = 'border border-transparent';
+            $hoverClass = "hover:bg-{$c}-500";
+            $focusRing = "focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{$c}-600";
+        }
     }
-    $focusRing = "focus:outline-none focus:ring-2 focus:ring-[rgba(var(--ui-{$baseVariant}-rgb),0.28)] focus:ring-offset-1 focus:ring-offset-[var(--ui-surface)]";
 
     // Größen & Icon-Skalierung
     if ($iconOnly) {
