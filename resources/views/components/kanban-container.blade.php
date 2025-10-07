1ed23518f4527356dce72835579067663c4e4d27
Martin Erren <martin3r@Mac.fritz.box>
2025-10-07 15:45:52 +0200

    Init

--- resources/views/components/kanban-container.blade.php
+++ resources/views/components/kanban-container.blade.php
@@ -5,9 +5,11 @@
 
 <div class="flex-1 min-h-0 overflow-x-auto">
     <x-ui-kanban-board 
-        @if($sortable) wire:sortable="{{ $sortable }}" @endif
-        @if($sortableGroup) wire:sortable-group="{{ $sortableGroup }}" @endif
-        class="h-full"
+        {{ $attributes->merge([
+            'class' => 'h-full',
+            'wire:sortable' => $sortable,
+            'wire:sortable-group' => $sortableGroup,
+        ]) }}
     >
         {{ $slot }}
     </x-ui-kanban-board>
