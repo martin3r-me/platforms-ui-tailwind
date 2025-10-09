9a2cc6aacc0fafbf9d9e66086813c209a19ac258
Martin Erren <martin3r@Mac.fritz.box>
2025-10-09 23:49:31 +0200

    Init

--- resources/views/components/kanban/column.blade.php
+++ resources/views/components/kanban/column.blade.php
@@ -11,6 +11,8 @@
         wire:key="column-{{ $sortableId }}"
     @endif
     {{ $attributes->merge(['class' => 'flex-shrink-0 h-full w-80 flex flex-col']) }}
+    x-data="{ isList: $parent.$parent.view === 'list' }"
+    :class="{ 'w-full': isList, 'w-80': !isList }"
 >
     <div class="flex flex-col h-full bg-white border border-gray-200 rounded-lg shadow-sm">
         
