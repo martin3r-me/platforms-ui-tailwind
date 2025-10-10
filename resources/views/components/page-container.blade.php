f58f2dd267985d5f009292352b5f53878b5ae817
Martin Erren <martin3r@Mac.fritz.box>
2025-10-10 21:53:27 +0200

    Init

--- resources/views/components/page-container.blade.php
+++ resources/views/components/page-container.blade.php
@@ -1,12 +1,11 @@
 @props([
-    'maxWidth' => 'max-w-none',
     'padding' => 'px-3 py-8',
     'spacing' => 'space-y-8',
     'background' => 'bg-gray-50/30',
 ])
 
 <div class="flex-1 overflow-y-auto {{ $background }}">
-    <div class="{{ $maxWidth }} {{ $padding }} {{ $spacing }}">
+    <div class="{{ $padding }} {{ $spacing }}">
         {{ $slot }}
     </div>
 </div>
