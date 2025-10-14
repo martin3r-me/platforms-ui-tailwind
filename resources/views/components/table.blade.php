f8c2792192402d20a6e0b5eea5e79cb27b63569b
Martin Erren <martin3r@Mac.fritz.box>
2025-10-14 14:52:46 +0200

    Init

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -5,8 +5,12 @@
     'compact' => false,
 ])
 
+@php
+    $tableTextSize = $compact ? 'text-sm' : 'text-base';
+@endphp
+
 <div class="bg-[color:var(--ui-surface)] rounded-lg border border-[color:var(--ui-border)] overflow-hidden shadow-sm">
-    <table class="w-full border-collapse">
+    <table class="w-full border-collapse {{ $tableTextSize }}">
         {{ $slot }}
     </table>
 </div>
