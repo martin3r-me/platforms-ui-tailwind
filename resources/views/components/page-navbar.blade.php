b5315a5d486e466a0f1633664cd6e6dfa4e5f60e
Martin Erren <martin3r@Mac.fritz.box>
2025-11-20 21:22:18 +0100

    init

--- resources/views/components/page-navbar.blade.php
+++ resources/views/components/page-navbar.blade.php
@@ -78,6 +78,12 @@
                 title="Zeit erfassen">
                 @svg('heroicon-o-clock', 'w-5 h-5')
             </button>
+            <button x-data
+                @click="$dispatch('keyresult:open')"
+                class="inline-flex items-center justify-center w-8 h-8 rounded-md border border-[var(--ui-border)]/60 transition text-[var(--ui-muted)] hover:text-[var(--ui-primary)] hover:bg-[var(--ui-muted-5)]"
+                title="KeyResult verknüpfen">
+                @svg('heroicon-o-flag', 'w-5 h-5')
+            </button>
 
             <div class="h-8 w-px bg-[var(--ui-border)]/60 mx-1"></div>
             {{-- Terminal Toggle (adjacent to Activity Toggle) --}}
