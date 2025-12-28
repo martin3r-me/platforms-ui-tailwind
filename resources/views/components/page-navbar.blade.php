92450c9f051fa7a546e1fd446a3eb45b76e68010
Martin Erren <martin3r@Mac.fritz.box>
2025-12-28 14:27:31 +0100

    kanban

--- resources/views/components/page-navbar.blade.php
+++ resources/views/components/page-navbar.blade.php
@@ -83,7 +83,7 @@
                 @click="$dispatch('keyresult:open')"
                 class="inline-flex items-center justify-center w-8 h-8 rounded-md border border-[var(--ui-border)]/60 transition text-[var(--ui-muted)] hover:text-[var(--ui-primary)] hover:bg-[var(--ui-muted-5)]"
                 title="KeyResult verknüpfen">
-                @svg('heroicon-o-target', 'w-5 h-5')
+                @svg('heroicon-o-viewfinder-circle', 'w-5 h-5')
             </button>
             <button x-data
                 @click="$dispatch('tagging:open')"
